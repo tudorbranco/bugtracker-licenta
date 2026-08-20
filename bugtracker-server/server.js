cf5c80270762612a33c23bac7069d83244034eae
tudorbranco <tudorbrancoveanu@gmail.com>
2026-08-20 19:29:05 +0300

    fix: schimbare interfata admin persoane

--- bugtracker-server/server.js
+++ bugtracker-server/server.js
@@ -8,7 +8,12 @@ require('dotenv').config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+  origin: ['https://bugtracker-licenta.vercel.app', 'http://localhost:5173', 'http://localhost:3000'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 const pool = new Pool(
   process.env.DATABASE_URL
